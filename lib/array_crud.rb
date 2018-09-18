92b25ce16b544a3d9f091f696da8e9673b667172
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-18 15:48:40 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,6 +11,10 @@ def add_element_to_end_of_array(array, element)
   [] << element
 end
 
+# def add_element_to_end_of_array(array, element)
+#   array << element  
+# end
+
 def add_element_to_start_of_array(array, element)
   array = []
   array.unshift(element)
